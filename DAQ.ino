2edb98a74a2d2e9e4885d0c2441e595fa9292421
marios <marioskritikakis@gmail.com>
2024-04-04 16:55:42 +0300

    firts revision

--- DAQ.ino
+++ DAQ.ino
@@ -3,17 +3,15 @@
 #include <Adafruit_Sensor.h>
 #include <SPI.h>
 #include <SD.h>
-
+#include "NMEA_Parser.h"
 #include <math.h>
 
 static uint8_t SupplyVoltage = 5;
-#define SupplyVoltageSense_Ratio 5.99;
-#define SupplyVoltageSense_Pin
-#define SupplyVoltage_Calc analogReference(INTERNAL);SupplyVoltage = SupplyVoltageSense_Ratio*analogRead(Pin);analogReference(DEFAULT);
+#define SupplyVoltageSense_Ratio 5.99
+#define SupplyVoltageSense_Pin 0
 
-
-#define adc_bit_width 10;
-#define ReadVoltage(Pin) SupplyVoltage*AnalogRead(Pin)/pow(2,adc_bit_width); 
+#define adc_bit_width 10
+#define ReadVoltage(Pin) (SupplyVoltage*analogRead(Pin)/pow(2,adc_bit_width))
 
 typedef struct information{
   uint8_t counter;
@@ -31,20 +29,21 @@ typedef struct information{
   float samplingRate;
   unsigned long elapsedTime;
   float elapsedTimeSeconds;
-  int potValue;
+  uint8_t potValue;
   float SpeedGPS;
 } log_t;
 
-void CalibrateSupplyVoltage(uint8_t Pin){
-  analogReference(INTERNAL);                // Choose 1.1V internal reference
-  SupplyVoltage = SupplyVoltageSense_Ratio*analogRead(Pin);
+
+void CalibrateSupplyVoltage(int Pin){
+  analogReference(INTERNAL1V1);             // Choose 1.1V internal reference
+  SupplyVoltage = SupplyVoltageSense_Ratio*ReadVoltage(SupplyVoltageSense_Pin);
   analogReference(DEFAULT);                 // Choose the default reference
 }
 
 Adafruit_MPU6050 mpu;
 File dataFile;
 
-const int startButtonPin = 2;
+static const int startButtonPin = 2;
 const int stopButtonPin = 3;
 const int resetButtonPin = 9;
 const int ledPin = 5;
@@ -99,25 +98,143 @@ void setup() {
   Serial1.begin(9600);        // Serial1 port connected to GPS
 }
 
-void readGPS(){
+void SerialPrintLog(log_t log){
+      // Print data to Serial
+      Serial.print("Counter: ");
+      Serial.print(log.counter);
+      Serial.print("\t");
+
+      Serial.print("AccX: ");
+      Serial.print(*log.accelerationX);
+      Serial.print("\t");
+
+      Serial.print("AccY: ");
+      Serial.print(*log.accelerationY);
+      Serial.print("\t");
+
+      Serial.print("AccZ: ");
+      Serial.print(*log.accelerationZ);
+      Serial.print("\t");
+
+      Serial.print("Roll: ");
+      Serial.print(log.roll);
+      Serial.print("\t");
+
+      Serial.print("Pitch: ");
+      Serial.print(log.pitch);
+      Serial.print("\t");
+
+      Serial.print("Yaw: ");
+      Serial.print(log.yaw);
+      Serial.print("\t");
+
+      Serial.print("GyroX: ");
+      Serial.print(*log.gyroX);
+      Serial.print("\t");
+
+      Serial.print("GyroY: ");
+      Serial.print(*log.gyroY);
+      Serial.print("\t");
+
+      Serial.print("GyroZ: ");
+      Serial.print(*log.gyroZ);
+      Serial.print("\t");
+
+      Serial.print("BrakePressure: ");
+      Serial.print(log.brakePressure);
+      Serial.print("\t");
+
+      Serial.print("TPS: ");
+      Serial.print(log.tps);
+      Serial.print("\t");
+
+      Serial.print("Sampling Rate: ");
+      Serial.print(log.samplingRate);
+      Serial.print(" Hz\t");
+
+      Serial.print("ElapsedTime: ");
+      Serial.print(log.elapsedTime);
+      Serial.print(" sec\t");
+
+      Serial.print("PotentiometerValue: ");
+      Serial.println(log.potValue);
+
+}
+
+void readGPS(log_t* log){
   if(Serial.available()){
+    int bufsize = 256;
+    char buf[bufsize];
     String something = Serial.readStringUntil('\n');      // Read the GPS data
-    // Serial.print(something);                           // Print the GPS data
+    
     something.toCharArray(buf,bufsize);                   // Convert the GPS data to char array
-    // Serial.print(buf);                                 // Print the char array           
+    
     packet_t packet = read(buf);
     
     if (packet.format == VTG)
-      // VTG_t message = packet.message.VTG;
-      Serial.print(packet.message.VTG.message.SpeedOverGround)
-      Serial.println("Km/h");
+      log->SpeedGPS = packet.message.VTG.SpeedOverGround;
   }
+}
+void readMPU6050(Adafruit_MPU6050 mpu ,log_t* log){
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  log->accelerationX = &a.acceleration.x;
+  log->accelerationY = &a.acceleration.y;
+  log->accelerationZ = &a.acceleration.z;
+  log->gyroX = &g.gyro.x;
+  log->gyroY = &g.gyro.y;
+  log->gyroZ = &g.gyro.z;
+}
+void readVariousSensors(log_t* log){
+  log->brakePressure = (analogRead(A2)-0.5);
+  log->tps = ReadVoltage(A1)/SupplyVoltage;
+  log->potValue = analogRead(potPin);
+}
+void Calc_Rotation_XYZ(log_t* log){
+  log->roll  = atan2(*log->accelerationY, *log->accelerationZ) * 180.0 / PI;
+  log->pitch = atan2(-(*log->accelerationX), sqrt(*log->accelerationY * (*log->accelerationY) + pow((*log->accelerationZ),2))) * 180.0 / PI;
+  log->yaw += (*log->gyroZ / 131.0) * (interval / 1000.0); // 131.0 is the sensitivity scale factor for the gyro (datasheet)
+}
+void WriteToSD(const log_t* log){
+  // Write data to SD card
+  dataFile.print(log->counter);
+  dataFile.print("\t");
+
+  dataFile.print(*log->accelerationX);
+  dataFile.print("\t");
+  dataFile.print(*log->accelerationY);
+  dataFile.print("\t");
+  dataFile.print(*log->accelerationZ);
+  dataFile.print("\t");
+  dataFile.print(log->roll);
+  dataFile.print("\t");
+  dataFile.print(log->pitch);
+  dataFile.print("\t");
+  dataFile.print(log->yaw);
+  dataFile.print("\t");
+  dataFile.print(*log->gyroX);
+  dataFile.print("\t");
+  dataFile.print(*log->gyroY);
+  dataFile.print("\t");
+  dataFile.print(*log->gyroZ);
+  dataFile.print("\t");
+  dataFile.print(log->brakePressure);
+  dataFile.print("\t");
+  dataFile.print(log->tps);
+  dataFile.print("\t");
+  dataFile.print(log->elapsedTime);
+  dataFile.print("\t");
+  dataFile.print(log->potValue);
+  dataFile.print(log->SpeedGPS);
+
+  dataFile.flush();
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  const log_t log;
+  static log_t *log;
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
@@ -137,15 +254,6 @@ void loop() {
     if (recording) {
       digitalWrite(ledPin, HIGH);
 
-      sensors_event_t a, g, temp;
-      mpu.getEvent(&a, &g, &temp);
-
-      log.accelerationX = &a.acceleration.x;
-      log.accelerationY = &a.acceleration.y;
-      log.accelerationZ = &a.acceleration.z;
-      log.roll = roll = atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI;
-      log.pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
-
       // float accelerationX = a.acceleration.x;
       // float accelerationY = a.acceleration.y;
       // float accelerationZ = a.acceleration.z;
@@ -156,121 +264,23 @@ void loop() {
       // gyroY = g.gyro.y;
       // gyroZ = g.gyro.z;
 
-      log.gyroX = g.gyro.x;
-      log.gyroY = g.gyro.y;
-      log.gyroZ = g.gyro.z;
-
+    
       // Calculate yaw using gyro readings
       // yaw += (gyroZ / 131.0) * (interval / 1000.0); // 131.0 is the sensitivity scale factor for the gyro (datasheet)
-      log.yaw += (gyroZ / 131.0) * (interval / 1000.0); // 131.0 is the sensitivity scale factor for the gyro (datasheet)
 
-      float brakePressure = analogRead(A2) * (4.5 / 1023) * 10.21; // Convert analog reading to brake pressure (in psi)
-      float tps = (analogRead(A1) / 1023.0) * 100; // Calculate TPS percentage
+      // float brakePressure = analogRead(A2) * (4.5 / 1023) * 10.21; // Convert analog reading to brake pressure (in psi)
+      // float tps = (analogRead(A1) / 1023.0) * 100; // Calculate TPS percentage
 
-      log.brakePressure = (analogRead(A2)-0.5)
+      
 
-      float samplingRate = 1000.0 / interval;
-      unsigned long elapsedTime = currentMillis;
-      float elapsedTimeSec = elapsedTime / 1000.0;
+      // float samplingRate = 1000.0 / interval;
+      float elapsedTimeSec = currentMillis / 1000.0;
 
-      // Read potentiometer value
-      int potValue = analogRead(potPin);
+      readMPU6050(mpu,log);
+      readGPS(log);                                            // Wait for VTG messsage and print speed in serial.  
+      readVariousSensors(log);
 
-
-      readGPS();                                            // Wait for VTG messsage and print speed in serial.  
-
-
-      // Print data to Serial
-      Serial.print("Counter: ");
-      Serial.print(counter);
-      Serial.print("\t");
-
-      Serial.print("AccX: ");
-      Serial.print(accelerationX);
-      Serial.print("\t");
-
-      Serial.print("AccY: ");
-      Serial.print(accelerationY);
-      Serial.print("\t");
-
-      Serial.print("AccZ: ");
-      Serial.print(accelerationZ);
-      Serial.print("\t");
-
-      Serial.print("Roll: ");
-      Serial.print(roll);
-      Serial.print("\t");
-
-      Serial.print("Pitch: ");
-      Serial.print(pitch);
-      Serial.print("\t");
-
-      Serial.print("Yaw: ");
-      Serial.print(yaw);
-      Serial.print("\t");
-
-      Serial.print("GyroX: ");
-      Serial.print(gyroX);
-      Serial.print("\t");
-
-      Serial.print("GyroY: ");
-      Serial.print(gyroY);
-      Serial.print("\t");
-
-      Serial.print("GyroZ: ");
-      Serial.print(gyroZ);
-      Serial.print("\t");
-
-      Serial.print("BrakePressure: ");
-      Serial.print(brakePressure);
-      Serial.print("\t");
-
-      Serial.print("TPS: ");
-      Serial.print(tps);
-      Serial.print("\t");
-
-      Serial.print("Sampling Rate: ");
-      Serial.print(samplingRate);
-      Serial.print(" Hz\t");
-
-      Serial.print("ElapsedTime: ");
-      Serial.print(elapsedTimeSec);
-      Serial.print(" sec\t");
-
-      Serial.print("PotentiometerValue: ");
-      Serial.println(potValue);
-
-      // Write data to SD card
-      dataFile.print(counter);
-      dataFile.print("\t");
-
-      dataFile.print(accelerationX);
-      dataFile.print("\t");
-      dataFile.print(accelerationY);
-      dataFile.print("\t");
-      dataFile.print(accelerationZ);
-      dataFile.print("\t");
-      dataFile.print(roll);
-      dataFile.print("\t");
-      dataFile.print(pitch);
-      dataFile.print("\t");
-      dataFile.print(yaw);
-      dataFile.print("\t");
-      dataFile.print(gyroX);
-      dataFile.print("\t");
-      dataFile.print(gyroY);
-      dataFile.print("\t");
-      dataFile.print(gyroZ);
-      dataFile.print("\t");
-      dataFile.print(brakePressure);
-      dataFile.print("\t");
-      dataFile.print(tps);
-      dataFile.print("\t");
-      dataFile.print(elapsedTimeSec);
-      dataFile.print("\t");
-      dataFile.println(potValue);
-
-      dataFile.flush();
+      WriteToSD(log);
     } else {
       digitalWrite(ledPin, LOW);
     }
