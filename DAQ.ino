321cfdd43a8c5fb133b36767a7ef379369b60dee
IamSynthetiC81 <marioskritikakis@gmail.com>
2024-04-27 05:36:36 +0300

    updated 27/04

--- DAQ.ino
+++ DAQ.ino
@@ -1,15 +1,17 @@
 #include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps612.h"
-#include "UBLOXgps.h"
-#include <FreqMeasure.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
-#include "NMEA_Parser.h"
+
 #include <math.h>
 #include <string.h>
 
+#include "MPU6050_6Axis_MotionApps612.h"
 #include "ErrorHandler.h"
+
+#include "UBLOXgps.h"
+#include "NMEA_Parser.h"
+
 
 #define SERIAL_LOGGIN true
 #define SD_LOGGIN false
@@ -20,9 +22,11 @@
  * @note The sampling frequency is set to 200Hz
  * @see initTimers()
 */
-#define __SAMPLING_RATE__ 200
+#define __SAMPLING_RATE__ 100
 
-/*  Button Declerations */
+/*  Button Declerations 
+ *  PIN 47 is used by Timer5 to measure pulses
+*/
 #define PIN_START_BUTTON 2
 #define PIN_STOP_BUTTON 3
 #define PIN_RESET_BUTTON 21
@@ -32,30 +36,15 @@
 #define PIN_TROTTLE_POSITION_SENSOR A3
 #define LED LED_BUILTIN
 
-/* COMUNICATION DECLERATION */
-#define __GPS_SERIAL__ Serial1
-#define __LOG_STREAM__ Serial
-
+static volatile bool WINDOW = false;
 volatile bool recording = false;
 volatile long StartTime = 0;
 
 static bool IMU_present = false;
 static bool SD_present = false;
-static volatile bool ERROR = false;
-/**
- * @brief Flag to indicate an error in the system
- *  01. Timer Overflow
- *  02. Aquisition Timed-Up
- *  03. DMP Init Fail
- *  04. IMU Init Fail
- *  05. SD  Init Fail
- *  06. GPS Fail Init
- */
-static volatile uint8_t ErrorFlag = false;
-static volatile bool WINDOW = false;
 
-MPU6050 mpu;
-File dataFile;    
+static MPU6050 mpu;
+static File dataFile;    
 
 /**
  * @brief Structure to store the data
@@ -87,6 +76,7 @@ typedef	struct information{
   uint16_t brakePressure;
   uint16_t tps;
   uint16_t potValue;
+  uint16_t CounterPulses;
   float	SpeedGPS;
   char Latitude[20];        // ddmm.mmmm
   uint8_t NSIndicator;      // N=north or S=south
@@ -111,9 +101,6 @@ log_t __LOG;
  * @brief Prints the log values to the Serial Monitor according to the format.
  */
 inline void __attribute__ ((always_inline))  SerialPrintLog(){
-    const PROGMEM uint8_t BufferSize = 256;
-    char buffer[BufferSize];
-
     Serial.print(__LOG.accelerationX,HEX);
     Serial.print(",");
     Serial.print(__LOG.accelerationY,HEX);
@@ -155,14 +142,14 @@ void MPU6050_init(){
   uint8_t devStatus;                                                          // return status after each device operation (0 = PASS, !0 = error)
   uint16_t packetSize;                                                        // expected DMP packet size (default is 42 bytes)
 
-  __LOG_STREAM__.print(F("Initializing I2C Drivers :"));
+  Serial.print(F("Initializing I2C Drivers :"));
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE                            // join I2C bus (I2Cdev library doesn't do this automatically)
     Wire.begin();
     Wire.setClock(400000);                                                    // 400kHz I2C clock. Comment this line if code is not compiling
-    __LOG_STREAM__.println(F("\t\tPASS"));
+    Serial.println(F("\t\tPASS"));
   #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
     Fastwire::setup(400, true);
-    __LOG_STREAM__.println(F("\t\tPASS"));
+    Serial.println(F("\t\tPASS"));
   #endif
 
   Wire.beginTransmission(0x68);                                               // Start I2C communication with the MPU6050
@@ -255,17 +242,35 @@ bool initTimers(uint16_t  freq){
   TCCR1B |= (1 << CS12);
   TIMSK1 |= (1 << OCIE1A);
 
+
+  /* Init timer 5 for use as a counter  */
+  TCCR5A = 0;                                                                   // Init Timer3
+  TCCR5B = 0;                                                                   // Init Timer3
+  TIMSK5 = 0;                                                                   // Timer/Counter3 Interrupt Mask Register
+  TCNT5 = 0;
+
+  
+  TCCR5A &= ~(1 << WGM50);                                                      // Normal
+  TCCR5A &= ~(1 << WGM51);                                                      // Normal
+  TCCR5B &= ~(1 << WGM51);                                                      // Normal
+  TCCR5B &= ~(1 << WGM52);                                                      // Normal
+  TCCR5B &= ~(1 << WGM53);                                                      // Normal
+
+  TCCR5B &= ~(1 << ICES5);                                                      // Falling edge trigger
+  
+  TCCR5B |= (1 << CS52);                                                        // Set CS32 to 1
+  TCCR5B |= (1 << CS51);                                                        // Set CS31 to 1          
+  TCCR5B &= ~(1 << CS50);                                                       // Set CS30 to 0        
+
   sei();                                                                        // Enable all interrupts                                                                 
 
   return true;  
 } 
 
-/**
- * 
-*/
 inline void __attribute__ ((always_inline))  readGPS(){
   __LOG.GPS_Valid = false;
-  if(__GPS_SERIAL__.available()){
+  if(Serial1.available()){
+
     int bufsize = 256;
     char buf[bufsize];
     packet_t packet;
@@ -275,13 +280,22 @@ inline void __attribute__ ((always_inline))  readGPS(){
     packet= read(buf);                                                          // Parse the GPS data
 
     if (packet.format == VTG){                                                  // Check if the data is a VTG message
+
       __LOG.SpeedGPS = packet.message.VTG.SpeedOverGround;                      // Get the speed from the GPS data
       __LOG.GPS_Valid = true;                                                   // Mark the GPS data as valid
+
     }else if (packet.format == GGA){                                            // Check if the data is a GGA message
+
+
+
       strcpy(__LOG.Latitude, packet.message.GGA.Latitude);
+
       strcpy(__LOG.Longitude, packet.message.GGA.Longitude);
+
       __LOG.NSIndicator = packet.message.GGA.NSIndicator;
+
       __LOG.EWIndicator = packet.message.GGA.EWIndicator;
+
       __LOG.FixQuality = packet.message.GGA.FixQuality;
 
       // Serial.print("Latitude : ");
@@ -293,17 +307,6 @@ inline void __attribute__ ((always_inline))  readGPS(){
 }
 
 /**
- * @brief Reads the data from the MPU6050 and stores it in the __LOG structure.
- * 
- * @note The data is stored in the __LOG structure and can be printed using SerialPrintLog() function
- * @see SerialPrintLog()
- * @see MPU6050_init()
-*/
-inline void __attribute__ ((always_inline))  readMPU6050(){
-  mpu.getMotion6(&__LOG.accelerationX, &__LOG.accelerationY, &__LOG.accelerationZ, &__LOG.gyroX, &__LOG.gyroY, &__LOG.gyroZ);
-}
-
-/**
  * @brief Reads the data from the various sensors and stores it in the __LOG structure.
  * 
  * @note The data is stored in the __LOG structure and can be printed using SerialPrintLog() function
@@ -312,10 +315,11 @@ inline void __attribute__ ((always_inline))  readMPU6050(){
  * 
 */
 inline void __attribute__ ((always_inline))  readVariousSensors(){
-  // __LOG.brakePressure = (ReadVoltage(A2)-0.5)*11.5;                           // Read the Brake Pressure
-  // __LOG.tps = ReadVoltage(A1)/SupplyVoltage;                                  // Read the Throttle Position Sensor
-  // __LOG.potValue	= ((analogRead(potPin)/1023.0)-0.5)*SteeringWheel_RotationalRange/2;  // Read the Potentiometer value
-
+  /* Math functions that need to b calculated for each sensor are the following :
+   * brakePressure = (ReadVoltage(A2)-0.5)*11.5;                           
+   * tps = ReadVoltage(A1)/SupplyVoltage;                                  
+   * potValue	= ((analogRead(potPin)/1023.0)-0.5)*SteeringWheel_RotationalRange/2;  
+  */
   analogReference(INTERNAL1V1);				                                        // Choose 1.1V internal reference
   __LOG.supplyVoltage = analogRead(PIN_SUPPLY_SENSE);                         // Read the supply voltage
   analogReference(DEFAULT);					                                          // Choose the default reference
@@ -323,6 +327,8 @@ inline void __attribute__ ((always_inline))  readVariousSensors(){
   __LOG.brakePressure = analogRead(PIN_BRAKE_PRESSURE);                       // Read the Brake Pressure
   __LOG.tps = analogRead(PIN_TROTTLE_POSITION_SENSOR);                        // Read the Throttle Position Sensor
   __LOG.potValue = analogRead(PIN_STEERING_WHEEL);                            // Read the Steering Wheel Position
+
+  __LOG.CounterPulses = TCNT5;                                                // Get the counter pulses
 }
 /**
  * @brief Writes the log values to the SD card in the format of a CSV file.
@@ -378,21 +384,30 @@ void ISR_stopRecording(){
 
   if(SD_present) dataFile.close();
 }
+
+/**
+ * @brief When the compare match occurs in Timer1, the ISR sets the window flag. When set, data  are sampled.
+ * 
+ * @note This is the interrupt service routine for the Timer1 compare match
+ * @see initTimers()
+ * 
+ */
 ISR(TIMER1_COMPA_vect){
   if (WINDOW){
-    VEC[ARD_ERROR_CODES] |= (1 << SAMPLING_RATE_LOW);
+    VEC[ERREG_ARD] |= (1 << SAMPLING_RATE_LOW);
   }
   WINDOW = !WINDOW;
 }
 
+
 void setup() {
   clearError();
 
-  __LOG_STREAM__.begin(115200);
+  Serial.begin(115200);
   while (!Serial && SERIAL_LOGGIN);
 
   /* Pin declerations */
-  __LOG_STREAM__.print(F("Initializing pins "));
+  Serial.print(F("Initializing pins "));
   pinMode(PIN_START_BUTTON, INPUT_PULLUP);
   pinMode(PIN_STOP_BUTTON, INPUT_PULLUP);
   pinMode(PIN_RESET_BUTTON, INPUT_PULLUP);
@@ -400,75 +415,59 @@ void setup() {
   digitalWrite(LED,HIGH);
   delay(500);
   digitalWrite(LED,LOW);
-  __LOG_STREAM__.println(F("\t\t\tPASS"));
+  Serial.println(F("\t\t\tPASS"));
 
-  __LOG_STREAM__.println(F("Initializing IMU :"));
+  Serial.println(F("Initializing IMU :"));
   MPU6050_init();                                                             // Initialize the MPU6050
   
   Serial.print(F("\nInitializing SD card :"));
   if (!SD.begin(53) && SD_LOGGIN) {                                           // SD card initialization
-    ERROR = true;
-    ErrorFlag = 0x05;
-    __LOG_STREAM__.println(F("SD initialization failed!\n\tFix and reboot to contrinue\n"));
+    // ERROR = true;
+    // ErrorFlag = 0x05;
+    Serial.println(F("SD initialization failed!\n\tFix and reboot to contrinue\n"));
     return;
   }
 
   dataFile = SD.open("data.txt", FILE_WRITE);                                 // Open the data file
-  dataFile ? __LOG_STREAM__.println(F("\t\t\tPASS")) : __LOG_STREAM__.println(F("\t\t\tFAIL"));
+  dataFile ? Serial.println(F("\t\t\tPASS")) : Serial.println(F("\t\t\tFAIL"));
 
-  __LOG_STREAM__.print(F("Initializing GPS :"));
+  Serial.print(F("Initializing GPS :"));
   initGPS();
-  __LOG_STREAM__.println("\t\t\tPASS");
+  Serial.println("\t\t\tPASS");
 
-  __LOG_STREAM__.print(F("Initializing FreqMeasure :"));
-  FreqMeasure.begin();                                                        // Initialize the FreqMeasure library
-  __LOG_STREAM__.println(F("\t\tPASS"));
-
-  __LOG_STREAM__.print(F("Setting up the interrupts :"));
+  Serial.print(F("Setting up the interrupts :"));
   attachInterrupt(digitalPinToInterrupt(PIN_START_BUTTON), ISR_startRecording, FALLING);
   attachInterrupt(digitalPinToInterrupt(PIN_STOP_BUTTON), ISR_stopRecording, FALLING);
-  __LOG_STREAM__.println(F("\t\tPASS "));
+  Serial.println(F("\t\tPASS "));
 
   Serial.print(F("Initializing Timers :"));
   if (initTimers(__SAMPLING_RATE__)){
-    __LOG_STREAM__.println(F("\t\tPASS"));
-    __LOG_STREAM__.print(" |--Sampling Frequency : ");
+    Serial.println(F("\t\tPASS"));
+    Serial.print(" |--Sampling Frequency : ");
   } else {
-    ERROR = true;
-    ErrorFlag = 0x01;
-    __LOG_STREAM__.println(F("\t\tFAIL"));
+    // ERROR = true;
+    // ErrorFlag = 0x01;
+    Serial.println(F("\t\tFAIL"));
 
   }
 
-  while(ERROR){};
+  // while(ERROR){};
 
-  __LOG_STREAM__.println(F("\n\t!!-Initialization complete-!!"));
+  Serial.println(F("\n\t!!-Initialization complete-!!"));
 }
 
 void loop()	{
   // if (!IMU_present) return;
   // if (!SD_present) return;
-
   if	(recording)	{
-
-    readGPS();			                                                          // Wait	for	VTG	messsage and print speed in	serial.	 
-
+    
+    readGPS();			                                                          // Wait	for	VTG	messsage and print speed in	Serial.	 
+    
     if (!WINDOW) return;
-
-    double sum = 0;
-    int count = 0;
-
-    // if(FreqMeasure.available()){
-    //   sum =+FreqMeasure.read();
-    //   count = count + 1;
-    //   if (count > 30){
-    //     float freq = FreqMeasure.countToFrequency(sum/count);
-    //     sum = 0;
-    //     count = 0;
-    //   }
-    // }
-  
-    readMPU6050();									                                          // Read data from	MPU6050
+    
+    /*  Read data from	MPU6050 */
+    mpu.getMotion6(&__LOG.accelerationX, &__LOG.accelerationY, &__LOG.accelerationZ, &__LOG.gyroX, &__LOG.gyroY, &__LOG.gyroZ);
+     
     readVariousSensors();
     
     if(SERIAL_LOGGIN) SerialPrintLog();                                         // Print the data
--- ErrorHandler.h
+++ ErrorHandler.h
@@ -1,15 +1,3 @@
-/**
- * @file ErrorHandler.h
- * 
- * @brief This file contains the error handling functions and error codes for the project.
- *  
-*/
-
-
-#include <stdint.h>
-#include <avr/pgmspace.h>
-
-
 static volatile uint8_t VEC[8];
 
 #define ERREG_GPS 0
@@ -21,48 +9,46 @@ static volatile uint8_t VEC[8];
 #define ERREG_GN1 6
 #define ERREG_GN2 7
 
-typedef enum GPS_ERROR_CODES{
+enum GPS_ERROR_CODES{
     BAUD_RATE_MISMATCH = 0,
     PORT_INACTIVE = 1,
-} GPS_ERROR_CODES;
+};
 
-typedef enum IMU_ERROR_CODES{
+enum IMU_ERROR_CODES{
     ADDRESS_NOT_FOUND = 0,
-} IMU_ERROR_CODES;
+};
 
-typedef enum SDC_ERROR_CODES{
+enum SDC_ERROR_CODES{
     FILE_NOT_FOUND = 0,
     SD_NOT_PRESENT = 1,
-} SDC_ERROR_CODES;
+};
 
-typedef enum ARD_ERROR_CODES{
+enum ARD_ERROR_CODES{
     SAMPLING_RATE_HIGH = 0,
     SAMPLING_RATE_LOW = 1,
-} ARD_ERROR_CODES;
+};
 
-typedef enum INT_ERROR_CODES{
+enum INT_ERROR_CODES{
     INTERRUPT_NOT_FOUND = 0,
     INTERRUPT_CORRUPT = 1,
     INTERRUPT_MISSING = 2,
     INTERRUPT_OUT_OF_BOUNDS = 3,
-} INT_ERROR_CODES;
+};
 
-typedef enum TIM_ERROR_CODES{
+enum TIM_ERROR_CODES{
     TIMER_UNAVAILABLE = 0,
     TIMER_OVERFLOW = 1,
-} TIM_ERROR_CODES;
+};
 
-typedef enum GN1_ERROR_CODES{
+enum GN1_ERROR_CODES{
     PROTOCOL_I2C_STUCK_BUS = 0,
     PROTOCOL_SERIAL_UNAVAILABLE = 1,
-} GN1_ERROR_CODES;
+};
 
 void clearError(){
-  size_t length = sizeof(VEC)/sizeof(*VEC);
+  size_t length = sizeof(VEC);
 
   for (int i = 0 ; i < length ; i++ ){
     VEC[i] = 0x00;
   }
 }
-
-void 
