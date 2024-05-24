2d0281e0493d63339cc3585029f6ac02876a1223
IamSynthetiC <66788358+IamSynthetiC81@users.noreply.github.com>
2024-05-24 22:15:00 +0300

    Fixed error on assert

--- src/general/src/general.h
+++ src/general/src/general.h
@@ -5,7 +5,7 @@
 #define EXIT_FAILURE 1
 
 #define assert(condition) if (!(condition));
-#define assert_msg(condition, message) if (!(condition));
+#define assert_msg(condition, message) if (!(condition)) Serial.println(message);
 
 // extern unsigned int _LINE;
 // extern char _FILE[256];
